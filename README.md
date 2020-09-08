97b0b2801bb9f6a6c9c9841bbfad84cb082dd1c4
alerner1 <alyssplerner@gmail.com>
2020-09-08 15:15:10 -0400

    races controller and basic views done

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 * add units to race length, elevation changes?
 * fix the stats table to actually match what a stat should look like
 
-runnerscontroller
-racescontroller
+runnerscontroller DONE
+  * still need to be able to see relationship related info, like stats, friends, registrations, etc
+racescontroller DONE
+  * still need to be able to see who's signed up etc
 registrationcontroller
 rankcategoriescontroller
 
---
+++ app/controllers/races_controller.rb
@@ -0,0 +1,59 @@
+class RacesController < ApplicationController
+  # don't need to be logged in to view all the races or a race's profile page
+  skip_before_action :authorized, only: [:index, :show] 
+  before_action :find_race, only: [:show, :destroy]
+
+  # i don't think you should be able to edit the race, only destroy it 
+  # and create a new one
+  # otherwise it's not fair to anyone who already signed up
+
+  #works
+  def index
+    @races = Race.all
+  end
+
+  def new
+    @race = Race.new
+  end
+
+  # works
+  def create
+    @race = Race.create(race_params)
+    
+    if @race.valid?
+      session[:race_id] = @race.id
+
+      flash[:success] = "Your race has been created!"
+      redirect_to race_path(@race)
+    else
+      flash[:errors] = @race.errors.full_messages
+      flash[:params] = params
+      redirect_to new_race_path
+    end    
+  end
+
+  # works
+  def show
+  end
+
+  # definitely need to make it so only the race's creator can destroy it
+  # and only if no one else has signed up yet
+  # somehow
+  # will probably need to add an attr to the race for the owner (runner_id, maybe as an alias or w/e)
+  def destroy
+    @race.destroy
+    
+    redirect_to races_path
+  end
+  
+
+  private
+
+  def find_race
+    @race = Race.find(params[:id])
+  end
+
+  def race_params
+    params.require(:race).permit(:race_type, :length, :elevation_increase, :elevation_decrease, :expiration_date)
+  end
+end
